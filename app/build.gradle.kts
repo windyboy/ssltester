e5e392b2aafc3fa06f15b8030ec960d1c04df2e8
windyboy <windyboy@gmail.com>
2025-05-19 15:14:54 +0800

    feat: add Gradle Shadow plugin for building fat JARs
    Include the 'com.gradleup.shadow' plugin to enable creating
    shadow (fat) JARs that bundle all dependencies. This simplifies
    deployment by producing a single executable artifact.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("com.gradleup.shadow") version "8.3.4"
 }
 
 repositories {
